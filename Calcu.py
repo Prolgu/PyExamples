3e38485f7df50d12901b070d47696efd1ef4af89
Prolgu <mjolner_@hotmail.com>
2020-03-25 18:19:22 -0300

    Agregado Calcu

---
+++ Calcu.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env phyton3
+# -*- coding: utf-8 -*-
+import os, sys
+
+def menuCalc():
+
+    os.system('clear')
+    print("Esto parece un menu:")
+    print("\t1 - Suma")
+    print("\t2 - Resta")
+    print("\t3 - Multiplicacion")
+    print("\t4 - Division")
+    print("\tq - Para salir")
+    
+def calculadora(calcu):
+	if calcu == "1":
+		os.system('clear')
+		s1=int(input("Ingrese un numero\n"))
+		s2=int(input("Ingrese otro\n"))
+		os.system('clear')
+		print("el resultado es: ",s1+s2)
+		input("\nPresione una tecla para continuar.")
+	elif calcu == "2":
+		os.system('clear')
+		s1=int(input("Ingrese un numero\n"))
+		s2=int(input("Ingrese otro\n"))
+		os.system('clear')
+		print("el resultado es: ",s1-s2)
+		input("\nPresione una tecla para continuar.")
+	elif calcu == "3":
+		os.system('clear')
+		s1=int(input("Ingrese un numero\n"))
+		s2=int(input("Ingrese otro\n"))
+		os.system('clear')
+		print("el resultado es: ",s1*s2)
+		input("\nPresione una tecla para continuar.")
+	elif calcu == "4":
+		os.system('clear')
+		s1=int(input("Ingrese un numero\n"))
+		s2=int(input("Ingrese otro\n"))
+		os.system('clear')
+		print("el resultado es: ", s1 / s2)
+		input("\nPresione una tecla para continuar.")
+	elif calcu == "q":
+		print("Gracias, Vuelva Prontoss")
+		sys.exit()
+	else:
+		os.system('clear')
+		print("Lo siento no es un numero valido!")
+
+while True:
+    
+    menuCalc()
+    calc = input("Ingrese su opcion: ")
+    calculadora(calc)
+	
+
+
