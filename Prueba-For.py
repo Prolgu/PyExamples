6ed22e9848b0a52d731abe3b8ef3f9bc5b959295
Prolgu <mjolner_@hotmail.com>
2020-03-25 23:49:29 -0300

    Cambios readme interno

--- Prueba-For.py
+++ Prueba-For.py
@@ -1,11 +1,27 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  Menu.py
-#
-#  Copyright 2020  Pr0LgU <https://github.com/Prolgu>
-#
+#  Prueba-For.py
+#  
+#  2020 Pr0LgU <https://github.com/Prolgu>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+# 
 print("Comienzo")
-for numero in range(10):
-    print(f"{numero} * {numero} = {numero**2} ")
-print(f"Final, numero vale: {numero} ")
+for i in range(10):
+    print(f"{i} * {i} = {i**2} ")
+print(f"Final, numero vale: {i} ")
